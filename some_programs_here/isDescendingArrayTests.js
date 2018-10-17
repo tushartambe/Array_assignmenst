17dd1020813fb4ca13b1392055ac9cbbf5dfbcbe
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2018-10-17 16:42:08 +0530

    one program and tests for it are added.
    Descending order - Given an array of numbers, check if the array is in descending order

--- some_programs_here/isDescendingArrayTests.js
+++ some_programs_here/isDescendingArrayTests.js
@@ -3,7 +3,8 @@ const assert = require('assert');
 
 assert.equal(isDescending([]),""); 
 assert.equal(isDescending([4]),true);
-assert.equal(isDescending([3,5]),true);
+assert.equal(isDescending([3,5]),false);
 assert.equal(isDescending([4,-2,5]),false);
-assert.equal(isDescending([2,0]),false);
+assert.equal(isDescending([2,0]),true);
+assert.equal(isDescending([5,2,1]),true);
 assert.equal(isDescending([23,405,12,0,75,9,7,2]),false);
