cb348d50bf8e9f926221fb292af53c39c859e35b
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2018-10-20 10:10:20 +0530

    All programs are in same directory now.

--- allTests.js
+++ allTests.js
@@ -1,242 +1,143 @@
 const assert = require('assert');
-//---------------------------------------
-const calculateAverage = require('./calculateAverageLib.js').calculateAverage;
+//---------------------------------
 
-//for empty array
-assert.equal(calculateAverage([0,-2,-4,-4]),-2.5); 
-//for only one value
-assert.equal(calculateAverage([3]),3);
-//for one digit values
-assert.equal(calculateAverage([3,5]),4);
-//for negative value
-assert.equal(calculateAverage([4,-2,4,2]),2);
-//for multiple digit numbers
-assert.equal(calculateAverage([23,40,1,0]),16);
-//-------------------------------------------------------------
+const uniqueElements = require('./arrayOfUniqueElementsLib.js').uniqueElements;
 
-const totalEvenNumbers = require('./countEvenNumbersLib.js').totalEvenNumbers;
+assert.deepEqual(uniqueElements([1]),[1]); 
 
-assert.deepEqual(totalEvenNumbers([]),0); 
-assert.deepEqual(totalEvenNumbers([4]),1);
-assert.deepEqual(totalEvenNumbers([3,5]),0);
-assert.deepEqual(totalEvenNumbers([4,-2,5]),2);
-assert.deepEqual(totalEvenNumbers([2,0]),2);
-assert.deepEqual(totalEvenNumbers([23,405,12,0,75,9,7,2]),3);
-//-------------------------------------------------------------
+assert.deepEqual(uniqueElements([2,4]),[2,4]);
 
-const totalOddNumbers = require('./countOddNumbersLib.js').totalOddNumbers;
+assert.deepEqual(uniqueElements([3,3]),[3]);
 
-//for empty array
-assert.deepEqual(totalOddNumbers([]),0); 
+assert.deepEqual(uniqueElements([2,3,4,2,3]),[2,3,4]);
 
-//for only one value
-assert.deepEqual(totalOddNumbers([3]),1);
+assert.deepEqual(uniqueElements([1,1,0,3,5,3,2]),[1,0,3,5,2]);
+//-----------------------------------------------------------------------
 
-//for one digit values
-assert.deepEqual(totalOddNumbers([3,5]),2);
-//for negative value
-assert.deepEqual(totalOddNumbers([4,-2,5]),1);
+const differentElements = require('./differentiateArraysLib.js').differentElements;
 
-//for all even numbers
-assert.deepEqual(totalOddNumbers([2,0]),0);
+assert.deepEqual(differentElements([1],[1]),[]); 
 
-assert.deepEqual(totalOddNumbers([23,405,12,0,75,9,7,2]),5);
-//-------------------------------------------------------------
+assert.deepEqual(differentElements([2,4],[3,2]),[4]);
 
-const countNumbersAbove = require('./countNumbersAboveThresholdLib.js').countNumbersAbove;
+assert.deepEqual(differentElements([3,3,5],[3,6,1,5,2]),[]);
 
-assert.deepEqual(countNumbersAbove([],4),0); 
-assert.deepEqual(countNumbersAbove([4],2),1);
-assert.deepEqual(countNumbersAbove([3,5],6),0);
-assert.deepEqual(countNumbersAbove([4,-2,5],0),2);
-assert.deepEqual(countNumbersAbove([2,0,2,1,],2),0);
-assert.deepEqual(countNumbersAbove([23,405,12,0,75,9,7,2],10),4);
-//-------------------------------------------------------------
+assert.deepEqual(differentElements([2,3,4,2,3],[3,6]),[2,4]);
 
-const countNumbersBelow = require('./countNumbersBelowThresholdLib.js').countNumbersBelow;
+assert.deepEqual(differentElements([1,1,0,3,5,3,7],[7,7,88]),[1,0,3,5]);
+//-----------------------------------------------------------------------
 
-assert.deepEqual(countNumbersBelow([],4),0); 
-assert.deepEqual(countNumbersBelow([4],2),0);
-assert.deepEqual(countNumbersBelow([3,5],4),1);
-assert.deepEqual(countNumbersBelow([4,-2,5],0),1);
-assert.deepEqual(countNumbersBelow([2,0,2,1,],2),2);
-assert.deepEqual(countNumbersBelow([23,405,12,0,75,9,7,2],10),4);
-//-------------------------------------------------------------
+const extractDigits = require('./extractDigitsLib.js').extractDigits;
 
-const findEvenNumbers = require('./findEvennumbersLib.js').findEvenNumbers;
+assert.deepEqual(extractDigits(2),[2]); 
+assert.deepEqual(extractDigits(45),[4,5]);
+assert.deepEqual(extractDigits(765),[7,6,5]);
+assert.deepEqual(extractDigits(87234),[8,7,2,3,4]);
+assert.deepEqual(extractDigits(65),[6,5]);
+//-----------------------------------------------------------------------
+const findFirstPosition = require('./fisrtPositionOfNumberLib.js').findFirstPosition;
 
-let inputArray = [];
-let outputArray = [];
-inputArray[1]=[2];
-outputArray[1] = [2];
-inputArray[2]=[3];
-outputArray[2] = [];
-inputArray[3]=[3,4,5];
-outputArray[3] = [4];
-inputArray[4]=[4,5];
-outputArray[4] = [4];
-inputArray[5]=[3,6,2,1,8,7];
-outputArray[5] = [6,2,8];
+assert.deepEqual(findFirstPosition([4],2),"");
+assert.deepEqual(findFirstPosition([3,5,6],6),2);
+assert.deepEqual(findFirstPosition([4,-2,5],0),"");
+assert.deepEqual(findFirstPosition([2,0,2,1,],2),0);
+assert.deepEqual(findFirstPosition([23,405,12,10,75,9,7,2],10),3);
+//-----------------------------------------------------------------------
 
-for(let index=1; index< inputArray.length; index++) {
-assert.deepEqual(findEvenNumbers(inputArray[index]),outputArray[index]);
-}
-//-------------------------------------------------------------
+const intersectionOfArrays = require('./intersectionOfArraysLib.js').intersectionOfArrays;
 
-const findOddNumbers = require('./findOddnumbersLib.js').findOddNumbers;
+assert.deepEqual(intersectionOfArrays([1],[2]),[]); 
 
-inputArray = [];
-outputArray = [];
-inputArray[1]=[3];
-outputArray[1] = [3];
-inputArray[2]=[4];
-outputArray[2] = [];
-inputArray[3]=[3,4,5];
-outputArray[3] = [3,5];
-inputArray[4]=[4,4];
-outputArray[4] = [];
-inputArray[5]=[3,6,2,1,8,7];
-outputArray[5] = [3,1,7];
+assert.deepEqual(intersectionOfArrays([2,4],[3]),[]);
 
-for(let index=1; index< inputArray.length; index++) {
-assert.deepEqual(findOddNumbers(inputArray[index]),outputArray[index]);
-}
-//---------------------------------------------------------------
-const findGreatestNumber = require('./findGreatestNumberLib.js').findGreatest;
+assert.deepEqual(intersectionOfArrays([3,3,5],[3,6,1,5,2]),[3,5]);
 
-//for empty array
-assert.equal(findGreatestNumber([-1,-2,-4,-4]),-1); 
-assert.equal(findGreatestNumber([]),undefined); 
-//for only one value
-assert.equal(findGreatestNumber([3]),3);
-//for one digit values
-assert.equal(findGreatestNumber([3,5]),5);
-//for negative value
-assert.equal(findGreatestNumber([4,-2,5]),5);
-//for multiple digit numbers
-assert.equal(findGreatestNumber([23,405,1,0]),405);
+assert.deepEqual(intersectionOfArrays([2,3,4,2,3],[3,6]),[3]);
 
-assert.equal(findGreatestNumber([23,405,Infinity,0]),Infinity);
+assert.deepEqual(intersectionOfArrays([1,1,0,3,5,3,7],[7,7,88]),[7]);
+//-----------------------------------------------------------------------
 
-assert.equal(findGreatestNumber([-Infinity,405,Infinity,0]),Infinity);
-//-------------------------------------------------------------
+const isAscending = require('./isAscendingArrayLib.js').isAscending;
 
-const findLowestNumber = require('./findLowestNumberLib.js').findLowest;
+assert.equal(isAscending([]),""); 
+assert.equal(isAscending([4]),true);
+assert.equal(isAscending([3,5]),true);
+assert.equal(isAscending([4,-2,5]),false);
+assert.equal(isAscending([2,0]),false);
+assert.equal(isAscending([23,405,12,0,75,9,7,2]),false);
+//-----------------------------------------------------------------------
 
-//for empty array
-assert.equal(findLowestNumber([-1,-2,-4,-4]),-4); 
-assert.equal(findLowestNumber([]),undefined); 
-//for only one value
-assert.equal(findLowestNumber([3]),3);
-//for one digit values
-assert.equal(findLowestNumber([3,5]),3);
-//for negative value
-assert.equal(findLowestNumber([4,-2,5]),-2);
-//for multiple digit numbers
-assert.equal(findLowestNumber([23,405,1,0]),0);
+const isDescending = require('./isDescendingArrayLib.js').isDescending;
 
-assert.equal(findLowestNumber([23,405,Infinity,10]),10);
+assert.equal(isDescending([]),""); 
+assert.equal(isDescending([4]),true);
+assert.equal(isDescending([3,5]),false);
+assert.equal(isDescending([4,-2,5]),false);
+assert.equal(isDescending([2,0]),true);
+assert.equal(isDescending([5,2,1]),true);
+assert.equal(isDescending([23,405,12,0,75,9,7,2]),false);
+//-----------------------------------------------------------------------
 
-assert.equal(findLowestNumber([-Infinity,405,Infinity,0]),-Infinity);
+const isSubset = require('./isSubsetLib.js').isSubset;
 
-//-------------------------------------------------------------
+assert.deepEqual(isSubset([1],[1]),false); 
 
-const findReverse = require('./findReverseOfArrayLib.js').findReverse;
+assert.deepEqual(isSubset([2,4],[3]),false);
+  
+assert.deepEqual(isSubset([3,5],[3]),true);
 
-inputArray = [];
-outputArray = [];
-inputArray[1]=[3];
-outputArray[1] = [3];
-inputArray[2]=[4,3];
-outputArray[2] = [3,4];
-inputArray[3]=[3,4,5];
-outputArray[3] = [5,4,3];
-inputArray[4]=[];
-outputArray[4] = [];
-inputArray[5]=[3,6,2,1,8,7];
-outputArray[5] = [7,8,1,2,6,3];
+assert.deepEqual(isSubset([2,3,4,2,3],[3,6]),false);
 
-for(let index=1; index< inputArray.length; index++) {
-assert.deepEqual(findReverse(inputArray[index]),outputArray[index]);
-}
-//-------------------------------------------------------------
+assert.deepEqual(isSubset([1,1,0,3,5,3,7],[7,1,0]),true);
+//-----------------------------------------------------------------------
 
-const findSum = require('./findSumOfArrayNumbersLib.js').findSum;
+const partitionOfArray = require('./partitionOfArrayLib.js').partitionOfArray;
 
-inputArray = [];
-outputArray = [];
-inputArray[1]=[3];
-outputArray[1] = 3;
-inputArray[2]=[4,3];
-outputArray[2] = 7;
-inputArray[3]=[3,4,5];
-outputArray[3] = 12;
-inputArray[4]=[];
-outputArray[4] ="";
-inputArray[5]=[3,6,2,1,8,7];
-outputArray[5] = 27;
+assert.deepEqual(partitionOfArray([1],1),[[1],[]]); 
 
-for(let index=1; index< inputArray.length; index++) {
-assert.deepEqual(findSum(inputArray[index]),outputArray[index]);
-}
-//-------------------------------------------------------------
+assert.deepEqual(partitionOfArray([2,4],4),[[2,4],[]]);
 
-const lengthOfEveryElement = require('./mappingLengthsLib.js').lengthOfEveryElement;
+assert.deepEqual(partitionOfArray([3,6,1,5,2],5),[[1,2,3,5],[6]]);
 
-//for empty string
-assert.deepEqual(lengthOfEveryElement([""]),[0]); 
+assert.deepEqual(partitionOfArray([2,3,4,2,3],2),[[2,2],[3,3,4]]);
 
-//for one string
-assert.deepEqual(lengthOfEveryElement(["tushar"]),[6]);
+assert.deepEqual(partitionOfArray([1,1,0,3,5,3,7],4),[[0,1,1,3,3],[5,7]]);
+//-----------------------------------------------------------------------
 
-//for 2 strings
-assert.deepEqual(lengthOfEveryElement(["hi","h"]),[2,1]);
+const rotateArray = require('./rotateArrayLib.js').rotateArray;
 
-//for multiple strings
-assert.deepEqual(lengthOfEveryElement(["hello","name","mr","kjsdads"]),[5,4,2,7]);
+assert.deepEqual(rotateArray([4],4),[4]);
 
-//for multiple combinations
-assert.deepEqual(lengthOfEveryElement(["h","","auy","abcde","34"]),[1,0,3,5,2]);
-//-------------------------------------------------------------
+assert.deepEqual(rotateArray([3,5,6],5),[6,3,5]);
 
-const fibSeries = require('./reverseFibSeriesLib.js').fibSeries;
+assert.deepEqual(rotateArray([4,5],5),[4,5]);
 
-inputArray = [];
-outputArray = [];
-inputArray[1]=2;
-outputArray[1] = [1,0];
-inputArray[2]=[3];
-outputArray[2] = [1,1,0];
-inputArray[3]=[];
-outputArray[3] = [];
-inputArray[4]=[5];
-outputArray[4] = [3,2,1,1,0];
-inputArray[5]=[8];
-outputArray[5] = [13,8,5,3,2,1,1,0];
+assert.deepEqual(rotateArray([2,0,3,1,],2),[0,3,1,2]);
 
-for(let index=1; index< inputArray.length; index++) {
-assert.deepEqual(fibSeries(inputArray[index]),outputArray[index]);
-}
+assert.deepEqual(rotateArray([34,23,22,43,4],22),[43,4,34,23,22]);
+//-----------------------------------------------------------------------
 
-//-------------------------------------------------------------
+const unionOfArrays = require('./unionOfArraysLib.js').unionOfArrays;
 
-const EverySecondNumbers = require('./selectEverySecondNumberLib.js').EverySecondNumbers;
+assert.deepEqual(unionOfArrays([1],[2]),[1,2]); 
 
-inputArray = [];
-outputArray = [];
-inputArray[1]=[3];
-outputArray[1] = [3];
-inputArray[2]=[4,3];
-outputArray[2] = [4];
-inputArray[3]=[3,4,5];
-outputArray[3] = [3,5];
-inputArray[4]=[];
-outputArray[4] = [];
-inputArray[5]=[3,6,2,1,8,7];
-outputArray[5] = [3,2,8];
+assert.deepEqual(unionOfArrays([2,4],[3]),[2,4,3]);
 
-for(let index=1; index< inputArray.length; index++) {
-assert.deepEqual(EverySecondNumbers(inputArray[index]),outputArray[index]);
-}
-//-------------------------------------------------------------
+assert.deepEqual(unionOfArrays([3,3,5],[3,6,1,5,2]),[3,5,6,1,2]);
 
+assert.deepEqual(unionOfArrays([2,3,4,2,3],[3,6]),[2,3,4,6]);
+
+assert.deepEqual(unionOfArrays([1,1,0,3,5,3,2],[7,7,88]),[1,0,3,5,2,7,88]);
+//-----------------------------------------------------------------------
+
+const zipOfArrays = require('./zipOfArraysLib.js').zipOfArrays;
+
+assert.deepEqual(zipOfArrays([1],[2]),[[1,2]]); 
+
+assert.deepEqual(zipOfArrays([2,4],[3]),[[2,3]]);
+
+assert.deepEqual(zipOfArrays([3,3,5],[3,6,1,5,2]),[[3,3],[6,3],[1,5]]);
+
+assert.deepEqual(zipOfArrays([2,3,4,2,3],[3,6,0]),[[2,3],[3,6],[4,0]]);
+
+assert.deepEqual(zipOfArrays([1,1,0,3,5,3,7],[7,7,88]),[[1,7],[1,7],[0,88]]);
