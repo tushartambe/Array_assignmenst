bfcce4d8c672ceef2beedfca9df3a420515026c7
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2018-10-23 15:36:02 +0530

    Refactor using inbuilt functions.
    Unnecessary lopps removed.
    reduce() function and object is used.

--- libs/fisrtPositionOfNumberLib.js
+++ libs/fisrtPositionOfNumberLib.js
@@ -1,11 +1,15 @@
-const findFirstPosition = function (array,number) {
-  let firstPosition = "";
-  for ( index =0; index < array.length; index++) {
-    if (array[index] == number) {
-      firstPosition = index;
-      index = array.length;
-    }
+const findPosition = function(state,element,index) {
+  if(state.element == element && state.index == -1) {
+    state.index = index;
+    return state;
   }
+  return state;
+}
+
+const findFirstPosition = function (array,number) {
+
+  let firstPosition = array.reduce(findPosition,{ element : number, index : -1 }).index;
+
   return firstPosition;
 }
 exports.findFirstPosition= findFirstPosition;
