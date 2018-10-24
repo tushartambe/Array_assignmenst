c7bc73a2429381734bf3bffad10b276329be9c42
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2018-10-24 11:47:06 +0530

    Lib files merged in one file. Test files aslo.
    All lib files are merged to make only one lib file.
    Same for tests. All tests are merged into one file.

---
+++ arrayLib.js
@@ -0,0 +1,361 @@
+const uniqueElements = function (numbers) {
+  let uniqueElementsArray = [];
+  for(let counter = 0; counter < numbers.length; counter++) {
+    if(!uniqueElementsArray.includes(numbers[counter])) {
+      uniqueElementsArray.push(numbers[counter]);
+    }
+  }
+  return uniqueElementsArray;
+}
+
+exports.uniqueElements = uniqueElements;
+//------------------------------------------
+
+const calculateSum = function (firstNumber,secondNumber) {
+  return firstNumber+secondNumber;
+}
+const calculateAverage = function (numbersList) {
+  return (numbersList.reduce(calculateSum,0))/numbersList.length;
+}
+exports.calculateAverage = calculateAverage;
+//--------------------------------------------------
+
+const totalEvenNumbers = function (numbers) {
+  return numbers.filter(function(element) { return (element % 2 == 0);}).length;
+
+}
+exports.totalEvenNumbers = totalEvenNumbers;
+//-----------------------------------------------------
+
+const checkGreaterNumbers = function(state, currentElement){
+  let {count,threshold}=state;
+
+  if(currentElement > state.threshold) {
+    state.count++
+  }
+  return state;
+}
+
+const countNumbersAbove = function (numbersList,threshold) {
+
+  let totalNumbers = numbersList.reduce(checkGreaterNumbers, {count : 0, threshold : threshold}).count;
+  return totalNumbers;
+}
+
+exports.countNumbersAbove = countNumbersAbove;
+//------------------------------------------------
+
+const checkLowerNumbers = function(countObject, currentElement){
+  let {count,threshold}=countObject;
+
+  if(currentElement < countObject.threshold) {
+    countObject.count++
+  }
+  return countObject;
+}
+
+const  countNumbersBelow = function (numbers,threshold) {
+
+  let totalNumbers = numbers.reduce(checkLowerNumbers, {count : 0, threshold : threshold}).count;
+  return totalNumbers;
+}
+
+exports.countNumbersBelow = countNumbersBelow;
+//---------------------------------------------------
+
+const totalOddNumbers = function (numbers) {
+  return numbers.filter(function(element) { return element % 2;}).length;
+}
+
+exports.totalOddNumbers = totalOddNumbers;
+//-----------------------------------------
+
+const differentElements = function (firstList,secondList) {
+  firstList = uniqueElements(firstList);
+  secondList = uniqueElements(secondList);
+
+  let isFound;
+  differentiatedElements = [];
+  for(let element of firstList) {
+    isFound = secondList.includes(element);
+
+    if(!isFound) {
+      differentiatedElements.push(element);
+    }
+  }
+
+  return differentiatedElements;
+}
+exports.differentElements = differentElements;
+//--------------------------------------------
+
+const extractDigits = function ( number) {
+  let convertedNumber = number.toString();
+  return  convertedNumber.split("");
+}
+
+exports.extractDigits = extractDigits;
+//---------------------------------------------
+
+const isEven  = function(element) {
+  return element % 2 == 0;
+}
+
+const findEvenNumbers = function (numbers) {
+  let evenNum = [];
+  evenNum =  numbers.filter(isEven);
+  return evenNum;
+}
+exports.findEvenNumbers = findEvenNumbers;
+//----------------------------------------------
+
+const bigNumber = function (firstNumber,secondNumber) {
+  return Math.max(firstNumber,secondNumber);
+}
+
+const findGreatestNumber = function (numbersList) {
+  return numbersList.reduce(bigNumber);
+}
+exports.findGreatest = findGreatestNumber;
+//----------------------------------------------
+
+const smallNumber = function (firstNumber,secondNumber) {
+  return Math.min(firstNumber,secondNumber);
+}
+
+const findLowestNumber = function (numbersList) {
+  return numbersList.reduce(smallNumber);
+}
+exports.findLowest = findLowestNumber;
+//-------------------------------------------------
+
+const isOdd = function (element) {
+  return element % 2;
+}
+
+const findOddNumbers = function (numbers) {
+  return numbers.filter(isOdd);
+}
+
+exports.findOddNumbers = findOddNumbers;
+//----------------------------------------------
+
+const findReverse = function (dataSet) {
+  return dataSet.reverse();
+}
+exports.findReverse= findReverse;
+//----------------------------------------------
+
+const sum = function(previousNumber,currentNumber) {
+  return previousNumber+currentNumber;
+}
+
+const findSum = function (dataSet) {
+  return dataSet.reduce(sum,0);
+}
+exports.findSum = findSum;
+//----------------------------------------------
+
+const findPosition = function(state,element,index) {
+  if(state.element == element && state.index == -1) {
+    state.index = index;
+    return state;
+  }
+  return state;
+}
+
+const findFirstPosition = function (numbersList,number) {
+
+  let firstPosition = numbersList.reduce(findPosition,{ element : number, index : -1 }).index;
+
+  return firstPosition;
+}
+exports.findFirstPosition= findFirstPosition;
+//----------------------------------------------
+
+const intersectionOfArrays = function (firstList,secondList) {
+  firstList = uniqueElements(firstList);
+  secondList = uniqueElements(secondList);
+
+  let intersection = [];
+  for(let element of secondList) {
+    if(firstList.includes(element)) {
+      intersection.push(element);
+    }
+  }
+
+  return intersection;
+}
+exports.intersectionOfArrays = intersectionOfArrays;
+//----------------------------------------------
+
+const isAscending = function (numbersList) {
+  for (let counter = 0; counter < numbersList.length - 1; counter++) {
+    if( numbersList[counter] > numbersList[counter + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+exports.isAscending = isAscending;
+//----------------------------------------------
+
+const isDescending = function (numbersList) {
+  for (let counter = 0; counter < numbersList.length - 1; counter++) {
+    if( numbersList[counter] < numbersList[counter + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+exports.isDescending = isDescending;
+//----------------------------------------------
+
+const isSubset = function (firstList,secondList) {
+  firstList = uniqueElements(firstList);
+  secondList = uniqueElements(secondList);
+
+  if( firstList.length <= secondList.length) {
+    return false;
+  }
+
+  for( let counter = 0; counter < secondList.length; counter++) {
+
+    if(!firstList.includes(secondList[counter])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+exports.isSubset = isSubset;
+//----------------------------------------------
+
+const lengthOfEveryElement = function (words) {
+  return words.map(function(element) { return element.length;});
+}
+
+exports.lengthOfEveryElement = lengthOfEveryElement;
+//----------------------------------------------
+
+const sortNumbers = function(numbersList) {
+  let swappingVariable;
+  for(let firstIndex = 0; firstIndex < numbersList.length; firstIndex++) {
+    for( let secondIndex =0; secondIndex < numbersList.length-1; secondIndex++) {
+      if( numbersList[secondIndex] > numbersList[secondIndex + 1]) {
+        swappingVariable = numbersList[secondIndex];
+        numbersList[secondIndex] = numbersList[secondIndex + 1];
+        numbersList[secondIndex + 1] = swappingVariable;
+      }
+    }
+  }
+  return numbersList;
+}
+
+const seperateNumbers = function (state,element){
+  let {smallerNumbers,biggerNumbers,number} = state;
+  if( element <= state.number) {
+    smallerNumbers = smallerNumbers.concat(element);
+    return {smallerNumbers :smallerNumbers,biggerNumbers:biggerNumbers,number:number};
+  }
+
+  biggerNumbers = biggerNumbers.concat(element);
+  return {smallerNumbers :smallerNumbers,biggerNumbers:biggerNumbers,number:number};
+}
+
+const partitionOfArray = function (numbersList,number) {
+  let sortedList = sortNumbers(numbersList);
+  let partitions = [];
+  partitions[0] = sortedList.reduce(seperateNumbers,{smallerNumbers:[],biggerNumbers: [],number:number}).smallerNumbers;
+  partitions[1] = sortedList.reduce(seperateNumbers,{smallerNumbers:[],biggerNumbers: [],number:number}).biggerNumbers;
+
+  return partitions;
+}
+exports.partitionOfArray = partitionOfArray;
+//----------------------------------------------
+
+const fibSeries = function (totalTerms) {
+  let firstTerm = -1;
+  let secondTerm = 1;
+  let nextTerm;
+  let fibSeriesNumbers = [];
+  for(let termIndex=0; termIndex<totalTerms; termIndex++){
+    nextTerm = firstTerm + secondTerm;
+    fibSeriesNumbers[termIndex] = nextTerm;
+    firstTerm = secondTerm;
+    secondTerm = nextTerm;
+  }
+  return fibSeriesNumbers.reverse();
+}
+exports.fibSeries = fibSeries;
+//----------------------------------------------
+
+const movePositions = function(dataSet,position) {
+  for(let counter = 0; counter <= position; counter++) {
+    let numberToMove = dataSet.shift();
+    dataSet.push(numberToMove);
+  }
+
+  return dataSet;
+}
+const rotateArray = function (dataSet,indexToRotate) {
+  return movePositions(dataSet,indexToRotate);
+}
+exports.rotateArray= rotateArray;
+//-----------------------------------------------
+const selectSecond = function (state,element){
+  let {index,elements} = state;
+  if(index % 2 == 0) {
+    elements =  elements.concat(element);
+  }
+  return {index : index+1,elements:elements}
+};
+
+const selectAlternateNumbers = function(numbersList) {
+  return numbersList.reduce(selectSecond,{index:0,elements:[]}).elements;
+}
+exports.selectAlternateNumbers= selectAlternateNumbers;
+//----------------------------------------------
+
+const unionOfArrays = function (firstDataSet,secondDataSet) {
+  firstDataSet = uniqueElements(firstDataSet);
+  secondDataSet = uniqueElements(secondDataSet);
+  let unionOfNumbers = [];
+
+  for(let counter = 0; counter < firstDataSet.length; counter++) {
+    if(!unionOfNumbers.includes(firstDataSet[counter])) {
+      unionOfNumbers.push(firstDataSet[counter]);
+    }
+  }
+
+  for(let counter = 0; counter < secondDataSet.length; counter++) {
+    if(!unionOfNumbers.includes(secondDataSet[counter])) {
+      unionOfNumbers.push(secondDataSet[counter]);
+    }
+  }
+
+  return unionOfNumbers;
+}
+exports.unionOfArrays = unionOfArrays;
+//----------------------------------------------
+
+const createZip = function(firstNumber,secondNumber) {
+  let zip = [];
+  zip[0] = firstNumber;
+  zip[1] = secondNumber;
+
+  return zip;
+}
+
+const zipOfArrays = function (firstDataSet,secondDataSet) {
+  let lengthOfSmallArray = Math.min(firstDataSet.length,secondDataSet.length);
+  let zip = [];
+  for( let counter = 0; counter < lengthOfSmallArray; counter++) {
+    zip[counter] = createZip(firstDataSet[counter],secondDataSet[counter]);
+  }
+
+  return zip;
+}
+exports.zipOfArrays = zipOfArrays;
---
+++ arrayLibTest.js
@@ -0,0 +1,263 @@
+const uniqueElements = require('./arrayLib.js').uniqueElements;
+const assert = require('assert');
+assert.deepEqual(uniqueElements([1]),[1]); 
+assert.deepEqual(uniqueElements([2,4]),[2,4]);
+assert.deepEqual(uniqueElements([3,3]),[3]);
+assert.deepEqual(uniqueElements([2,3,4,2,3]),[2,3,4]);
+assert.deepEqual(uniqueElements([1,1,0,3,5,3,2]),[1,0,3,5,2]);
+
+//----------------------------------------------
+
+const calculateAverage = require('./arrayLib.js').calculateAverage;
+//for empty array
+assert.equal(calculateAverage([0,-2,-4,-4]),-2.5); 
+//for only one value
+assert.equal(calculateAverage([3]),3);
+//for one digit values
+assert.equal(calculateAverage([3,5]),4);
+//for negative value
+assert.equal(calculateAverage([4,-2,4,2]),2);
+//for multiple digit numbers
+assert.equal(calculateAverage([23,40,1,0]),16);
+
+//----------------------------------------------
+
+const totalEvenNumbers = require('./arrayLib.js').totalEvenNumbers;
+
+assert.deepEqual(totalEvenNumbers([]),0); 
+assert.deepEqual(totalEvenNumbers([4]),1);
+assert.deepEqual(totalEvenNumbers([3,5]),0);
+assert.deepEqual(totalEvenNumbers([4,-2,5]),2);
+assert.deepEqual(totalEvenNumbers([2,0]),2);
+assert.deepEqual(totalEvenNumbers([23,405,12,0,75,9,7,2]),3);
+//----------------------------------------------
+
+const countNumbersAbove = require('./arrayLib.js').countNumbersAbove;
+
+assert.deepEqual(countNumbersAbove([],4),0); 
+assert.deepEqual(countNumbersAbove([4],2),1);
+assert.deepEqual(countNumbersAbove([3,5],6),0);
+assert.deepEqual(countNumbersAbove([4,-2,5],0),2);
+assert.deepEqual(countNumbersAbove([2,0,2,1,],2),0);
+assert.deepEqual(countNumbersAbove([23,405,12,0,75,9,7,2],10),4);
+//----------------------------------------------
+
+const countNumbersBelow = require('./arrayLib.js').countNumbersBelow;
+
+assert.deepEqual(countNumbersBelow([],4),0); 
+assert.deepEqual(countNumbersBelow([4],2),0);
+assert.deepEqual(countNumbersBelow([3,5],4),1);
+assert.deepEqual(countNumbersBelow([4,-2,5],0),1);
+assert.deepEqual(countNumbersBelow([2,0,2,1,],2),2);
+assert.deepEqual(countNumbersBelow([23,405,12,0,75,9,7,2],10),4);
+//----------------------------------------------
+
+const totalOddNumbers = require('./arrayLib.js').totalOddNumbers;
+//for empty array
+assert.deepEqual(totalOddNumbers([]),0); 
+//for only one value
+assert.deepEqual(totalOddNumbers([3]),1);
+//for one digit values
+assert.deepEqual(totalOddNumbers([3,5]),2);
+//for negative value
+assert.deepEqual(totalOddNumbers([4,-2,5]),1);
+//for all even numbers
+assert.deepEqual(totalOddNumbers([2,0]),0);
+assert.deepEqual(totalOddNumbers([23,405,12,0,75,9,7,2]),5);
+//----------------------------------------------
+
+const differentElements = require('./arrayLib.js').differentElements;
+
+assert.deepEqual(differentElements([1],[1]),[]); 
+assert.deepEqual(differentElements([2,4],[3,2]),[4]);
+assert.deepEqual(differentElements([3,3,5],[3,6,1,5,2]),[]);
+assert.deepEqual(differentElements([2,3,4,2,3],[3,6]),[2,4]);
+assert.deepEqual(differentElements([1,1,0,3,5,3,7],[7,7,88]),[1,0,3,5]);
+//----------------------------------------------
+
+const extractDigits = require('./arrayLib.js').extractDigits;
+
+assert.deepEqual(extractDigits(2),[2]); 
+assert.deepEqual(extractDigits(45),[4,5]);
+assert.deepEqual(extractDigits(765),[7,6,5]);
+assert.deepEqual(extractDigits(87234),[8,7,2,3,4]);
+assert.deepEqual(extractDigits(65),[6,5]);
+//----------------------------------------------
+
+const findEvenNumbers = require('./arrayLib.js').findEvenNumbers;
+
+assert.deepEqual(findEvenNumbers([2]),[2]);
+assert.deepEqual(findEvenNumbers([3]),[]);
+assert.deepEqual(findEvenNumbers([3,4,5]),[4]);
+assert.deepEqual(findEvenNumbers([4,5]),[4]);
+assert.deepEqual(findEvenNumbers([3,6,2,1,8,7]),[6,2,8]);
+//----------------------------------------------
+
+const findGreatestNumber = require('./arrayLib.js').findGreatest;
+
+//for empty array
+assert.equal(findGreatestNumber([-1,-2,-4,-4]),-1); 
+//for only one value
+assert.equal(findGreatestNumber([3]),3);
+//for one digit values
+assert.equal(findGreatestNumber([3,5]),5);
+//for negative value
+assert.equal(findGreatestNumber([4,-2,5]),5);
+//for multiple digit numbers
+assert.equal(findGreatestNumber([23,405,1,0]),405);
+assert.equal(findGreatestNumber([23,405,Infinity,0]),Infinity);
+assert.equal(findGreatestNumber([-Infinity,405,Infinity,0]),Infinity);
+//---------------------------------------------------
+
+const findLowestNumber = require('./arrayLib.js').findLowest;
+
+//for empty array
+assert.equal(findLowestNumber([-1,-2,-4,-4]),-4); 
+//for only one value
+assert.equal(findLowestNumber([3]),3);
+//for one digit values
+assert.equal(findLowestNumber([3,5]),3);
+//for negative value
+assert.equal(findLowestNumber([4,-2,5]),-2);
+//for multiple digit numbers
+assert.equal(findLowestNumber([23,405,1,0]),0);
+assert.equal(findLowestNumber([23,405,Infinity,10]),10);
+assert.equal(findLowestNumber([-Infinity,405,Infinity,0]),-Infinity);
+
+//------------------------------------------------
+const findOddNumbers = require('./arrayLib.js').findOddNumbers;
+
+assert.deepEqual(findOddNumbers([3]),[3]);
+assert.deepEqual(findOddNumbers([4]),[]);
+assert.deepEqual(findOddNumbers([3,4,5]),[3,5]);
+assert.deepEqual(findOddNumbers([4,4]),[]);
+assert.deepEqual(findOddNumbers([3,6,2,1,8,7]),[3,1,7]);
+
+//------------------------------------------------------
+const findReverse = require('./arrayLib.js').findReverse;
+
+assert.deepEqual(findReverse([3]),[3]);
+assert.deepEqual(findReverse([4,3]),[3,4]);
+assert.deepEqual(findReverse([3,4,5]),[5,4,3]);
+assert.deepEqual(findReverse([3,6,2,1,8,7]),[7,8,1,2,6,3]);
+//---------------------------------------------------------
+
+const findSum = require('./arrayLib.js').findSum;
+
+assert.deepEqual(findSum([3]),3);
+assert.deepEqual(findSum([4,3]),7);
+assert.deepEqual(findSum([3,4,5]),12);
+assert.deepEqual(findSum([3,6,2,1,8,7]),27);
+
+//---------------------------------------------------------
+const findFirstPosition = require('./arrayLib.js').findFirstPosition;
+
+assert.deepEqual(findFirstPosition([4],2),-1);
+assert.deepEqual(findFirstPosition([3,5,6],6),2);
+assert.deepEqual(findFirstPosition([4,-2,5],0),-1);
+assert.deepEqual(findFirstPosition([2,0,2,1,],2),0);
+assert.deepEqual(findFirstPosition([23,405,12,10,75,9,7,2],10),3);
+//----------------------------------------------------------
+const intersectionOfArrays = require('./arrayLib.js').intersectionOfArrays;
+
+assert.deepEqual(intersectionOfArrays([1],[2]),[]); 
+assert.deepEqual(intersectionOfArrays([2,4],[3]),[]);
+assert.deepEqual(intersectionOfArrays([3,3,5],[3,6,1,5,2]),[3,5]);
+assert.deepEqual(intersectionOfArrays([2,3,4,2,3],[3,6]),[3]);
+assert.deepEqual(intersectionOfArrays([1,1,0,3,5,3,7],[7,7,88]),[7]);
+//--------------------------------------------------------
+
+const isAscending = require('./arrayLib.js').isAscending;
+
+assert.equal(isAscending([4]),true);
+assert.equal(isAscending([3,5]),true);
+assert.equal(isAscending([4,-2,5]),false);
+assert.equal(isAscending([2,0]),false);
+assert.equal(isAscending([23,405,12,0,75,9,7,2]),false);
+//-----------------------------------------------------------
+
+const isDescending = require('./arrayLib.js').isDescending;
+
+assert.equal(isDescending([4]),true);
+assert.equal(isDescending([3,5]),false);
+assert.equal(isDescending([4,-2,5]),false);
+assert.equal(isDescending([2,0]),true);
+assert.equal(isDescending([5,2,1]),true);
+assert.equal(isDescending([23,405,12,0,75,9,7,2]),false);
+//----------------------------------------------------------
+
+const isSubset = require('./arrayLib.js').isSubset;
+
+assert.deepEqual(isSubset([1],[1]),false); 
+assert.deepEqual(isSubset([2,4],[3]),false);
+assert.deepEqual(isSubset([3,5],[3]),true);
+assert.deepEqual(isSubset([2,3,4,2,3],[3,6]),false);
+assert.deepEqual(isSubset([1,1,0,3,5,3,7],[7,1,0]),true);
+//--------------------------------------------------------------
+
+const lengthOfEveryElement = require('./arrayLib.js').lengthOfEveryElement;
+//for empty string
+assert.deepEqual(lengthOfEveryElement([""]),[0]); 
+//for one string
+assert.deepEqual(lengthOfEveryElement(["tushar"]),[6]);
+//for 2 strings
+assert.deepEqual(lengthOfEveryElement(["hi","h"]),[2,1]);
+//for multiple strings
+assert.deepEqual(lengthOfEveryElement(["hello","name","mr","kjsdads"]),[5,4,2,7]);
+//for multiple combinations
+assert.deepEqual(lengthOfEveryElement(["h","","auy","abcde","34"]),[1,0,3,5,2]);
+
+//----------------------------------------------------------------
+
+const partitionOfArray = require('./arrayLib.js').partitionOfArray;
+
+assert.deepEqual(partitionOfArray([1],1),[[1],[]]); 
+assert.deepEqual(partitionOfArray([2,4],4),[[2,4],[]]);
+assert.deepEqual(partitionOfArray([3,6,1,5,2],5),[[1,2,3,5],[6]]);
+assert.deepEqual(partitionOfArray([2,3,4,2,3],2),[[2,2],[3,3,4]]);
+assert.deepEqual(partitionOfArray([1,1,0,3,5,3,7],4),[[0,1,1,3,3],[5,7]]);
+//--------------------------------------------------------------------
+
+const fibSeries = require('./arrayLib.js').fibSeries;
+
+assert.deepEqual(fibSeries(2),[1,0]);
+assert.deepEqual(fibSeries([3]),[1,1,0]);
+assert.deepEqual(fibSeries([]),[]);
+assert.deepEqual(fibSeries([5]),[3,2,1,1,0]);
+assert.deepEqual(fibSeries([8]),[13,8,5,3,2,1,1,0]);
+//----------------------------------------------------------
+
+const rotateArray = require('./arrayLib.js').rotateArray;
+
+assert.deepEqual(rotateArray([4],4),[4]);
+assert.deepEqual(rotateArray([3,5,6],1),[6,3,5]);
+assert.deepEqual(rotateArray([4,5],1),[4,5]);
+assert.deepEqual(rotateArray([2,0,3,1,],0),[0,3,1,2]);
+assert.deepEqual(rotateArray([34,23,22,43,4],2),[43,4,34,23,22]);
+//-----------------------------------------------------------
+
+const selectAlternateNumbers = require('./arrayLib.js').selectAlternateNumbers;
+
+assert.deepEqual(selectAlternateNumbers([3]),[3]);
+assert.deepEqual(selectAlternateNumbers([4,3]),[4]);
+assert.deepEqual(selectAlternateNumbers([3,4,5]),[3,5]);
+assert.deepEqual(selectAlternateNumbers([]),[]);
+assert.deepEqual(selectAlternateNumbers([3,6,2,1,8,7]),[3,2,8]);
+//-------------------------------------------------------
+
+const unionOfArrays = require('./arrayLib.js').unionOfArrays;
+
+assert.deepEqual(unionOfArrays([1],[2]),[1,2]); 
+assert.deepEqual(unionOfArrays([2,4],[3]),[2,4,3]);
+assert.deepEqual(unionOfArrays([3,3,5],[3,6,1,5,2]),[3,5,6,1,2]);
+assert.deepEqual(unionOfArrays([2,3,4,2,3],[3,6]),[2,3,4,6]);
+assert.deepEqual(unionOfArrays([1,1,0,3,5,3,2],[7,7,88]),[1,0,3,5,2,7,88]);
+//------------------------------------------------------------------
+
+const zipOfArrays = require('./arrayLib.js').zipOfArrays;
+
+assert.deepEqual(zipOfArrays([1],[2]),[[1,2]]); 
+assert.deepEqual(zipOfArrays([2,4],[3]),[[2,3]]);
+assert.deepEqual(zipOfArrays([3,3,5],[3,6,1,5,2]),[[3,3],[3,6],[5,1]]);
+assert.deepEqual(zipOfArrays([2,3,4,2,3],[3,6,0]),[[2,3],[3,6],[4,0]]);
+assert.deepEqual(zipOfArrays([1,1,0,3,5,3,7],[7,7,88]),[[1,7],[1,7],[0,88]]);
